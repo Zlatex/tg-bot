2fb98652a76b3abd92d69dcba5a8daad37c7cf92
Zlatex <maximzlatogorsky@gmail.com>
2020-12-28 15:38:05 +0200

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup, Extra } = require('telegraf');
 const express = require('express')
 const app = express();
 var bodyParser = require('body-parser');
@@ -6,9 +6,11 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 app.use(bodyParser.json());
 
 app.post('/send-notify', (request, response) => {
-    response.send(request.body);
-  console.log(request.body);
-
+    const info = request.body;
+    const keyboard = Markup.inlineKeyboard([
+        Markup.urlButton(info.urlText ? info.urlText : "Go to site", info.url)
+    ])
+    bot.telegram.sendMessage(info.to,info.message,info.url ? Extra.markup(keyboard) : {});
+    response.sendStatus(200);
 })
-
 app.listen(3131, () => {console.log('Marketplace app listening on port 3131!')})
